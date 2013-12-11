f320f61cade8cfdac66d5a4c7a716cde770cae9f
Albert Akhmetov <albert.akhmetov@hotmail.com>
2013-12-12 00:25:38 +0600

    Add "AddTest"

---
+++ sources/Collections.Tests/SortedCollectionTests.cs
@@ -0,0 +1,35 @@
+﻿/* Copyright © 2013, Albert Akhmetov (albert.akhmetov@hotmail.com) */
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Specialized;
+using System.Linq;
+
+namespace Collections
+{
+    [TestClass]
+    public class SortedCollectionTests
+    {
+        [TestMethod]
+        public void AddTest()
+        {
+            var item = 1;
+            var wasInvoke = false;
+
+            var sortedCollection = new SortedCollection();
+            sortedCollection.CollectionChanged += (x, y) =>
+                {
+                    wasInvoke = true;
+
+                    Assert.AreEqual(NotifyCollectionChangedAction.Add, y.Action);
+                    Assert.AreEqual(1, y.NewItems.Count);
+                    Assert.AreEqual(item, y.NewItems[0]);
+                };
+
+            sortedCollection.Add(item);
+
+            Assert.AreEqual(1, sortedCollection.Count); 
+            Assert.IsTrue(wasInvoke);           
+        }
+    }
+}
---
+++ sources/Collections/SortedCollection.cs
@@ -0,0 +1,32 @@
+﻿/* Copyright © 2013, Albert Akhmetov (albert.akhmetov@hotmail.com) */
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    public class SortedCollection : INotifyCollectionChanged
+    {
+        public void Add(int item)
+        {
+            OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, item));
+        }
+
+        public int Count
+        {
+            get { return 1; }             
+        }
+
+        protected void OnCollectionChanged(NotifyCollectionChangedEventArgs e)
+        {
+            if (CollectionChanged != null)
+                CollectionChanged(this, e);
+        }
+
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
+    }
+}
